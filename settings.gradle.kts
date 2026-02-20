0c0fc1d9ec308fc77b63f1d25857caa5eb2c7f4e
Ranbato <ranbato@gmail.com>
2026-02-20 06:25:01 -0700

    Expanded example and updated README.md

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
+}
 rootProject.name = "ledsign3"
-
